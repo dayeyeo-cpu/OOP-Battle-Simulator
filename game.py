43f2edeb1f95d81cf072282748d4ab9b1af3978b
dayeyeo-cpu <daye.yeo@ascte.org>
2026-09-14 20:01:31 -0500

    Added the hero

--- game.py
+++ game.py
@@ -1,8 +1,14 @@
+from hero import Hero
+
 from goblin import Goblin
 
 
 ARENA_NAME = "The Labubu Realm"
 
+ 
+"""Have the Hero attack one Goblin. Save the returned damage in a variable.
+    Pass that damage to the Goblin's take_damage() method.
+    If the Goblin is still alive, allow it to attack the Hero once."""
 
 def main():
     """Open the arena and introduce its first opponent."""
@@ -11,13 +17,24 @@ def main():
     print("The gates are opening...")
 
     goblin = Goblin("Tripe T")
+    secondgoblin = Goblin("Labubu")
+    hero = Hero("BIBBLE")
 
     print(f"{goblin.name} enters the arena with {goblin.health} health.")
-    secondgoblin = Goblin("Labubu")
     
     print(f"{secondgoblin.name} enters the arena with {secondgoblin.health} health.")
-    print("But no hero has answered the call... yet.")
+
+    print(f"{hero.name} enters the arena with {hero.health} health.")
 
 
+    bibble = Hero("BIBBLE)")
+    heroAttack = bibble.attack()
+    goblin.take_damage(heroAttack)
+
+    print("Bibble attacks ")
+    tt = Goblin("Tripe T")
+    goblinAttack = tt.attack()
+    print("Tripe T a")
+    hero.take_damage(goblinAttack)
 if __name__ == "__main__":
     main()
--- hero.py
+++ hero.py
@@ -1,4 +1,22 @@
+import random
+
 class Hero:
     """The hero blueprint will be implemented later in the project."""
 
-    pass
+    def __init__(self, name):
+        self.name = name
+        self.health = 120
+        self.attack_power = 22
+        self.armor = 5
+
+    def attack(self):
+        # Return a random value from 1 through this Hero's attack power.
+        return random.randint(1, self.attack_power)
+    def take_damage(self, damage):
+        # Subtract damage, but do not allow health to fall below 0.
+        self.health = max(0, self.health - damage)
+        print(f"{self.name} takes {damage} damage. Health: {self.health}")
+        
+    def is_alive(self):
+        # Return a Boolean based on this Hero's health.
+        return self.health >0
